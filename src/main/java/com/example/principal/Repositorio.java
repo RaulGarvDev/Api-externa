f92997629520e82a04c8b6d3b229aa71325ffa15
RaulGarvDev <raulgarvdev@gmail.com>
2022-07-12 20:52:11 +0200

    new file:   src/main/java/com/example/principal/Controlador.java 	new file:   src/main/java/com/example/principal/Repositorio.java 	new file:   src/main/java/com/example/principal/ServicioSuper.java 	new file:   src/main/java/com/example/principal/ServicioSuper2.java 	new file:   src/main/java/com/example/principal/SuperHero.java

---
+++ src/main/java/com/example/principal/Repositorio.java
@@ -0,0 +1,14 @@
+package com.example.principal;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface Repositorio extends CrudRepository<SuperHero, Integer>{
+
+
+} 
+    
+
---
+++ src/main/java/com/example/principal/ServicioSuper.java
@@ -0,0 +1,10 @@
+package com.example.principal;
+
+import java.util.List;
+
+public interface ServicioSuper {
+    
+    public List <SuperHero> getTask();
+    public SuperHero superHeroCreate(SuperHero a);
+
+}
